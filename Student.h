f7a88a53c130be1ba7deabbbd577ac58da0411f1
sunzhijuns <2843359242@qq.com>
2017-11-22 13:25:21 +0800

    选择排序模板化

---
+++ Student.h
@@ -0,0 +1,16 @@
+#pragma once
+#include<iostream>
+#include<string>
+using namespace std;
+struct Student
+{
+	string name;
+	int score;
+	bool operator<(const Student &otherStudent) {
+		return score < otherStudent.score;
+	}
+	friend ostream& operator<<(ostream &os, const Student &student) {
+		os << "Student: " << student.name << " " << student.score << endl;
+		return os;
+	}
+};
--- main.cpp
+++ main.cpp
@@ -3,15 +3,17 @@
 
 #include "stdafx.h"
 #include<iostream>
+#include"Student.h"
 //#include<algorithm>
 using namespace std;
-void selectionSort(int arr[], int n) {
+template<typename T>
+void selectionSort(T arr[], int n) {
 	for (int i = 0; i < n - 1; i++)
 	{
 		int minIndex = i;
 		for (int j = i + 1; j < n; j++)
 		{
-			if (arr[minIndex] > arr[j])
+			if (arr[j]<arr[minIndex] )
 			{
 				minIndex = j;
 			}
@@ -29,9 +31,34 @@ int main()
 {
 	int arr[5] = { 1,2,3,1,2 };
 	selectionSort(arr, 5);
-	for (size_t i = 0; i < 5; i++)
+	for (int i = 0; i < 5; i++)
 	{
 		cout << arr[i] << " ";
+	}
+	cout << endl;
+
+	float b[3] = {1.0,0.0,4.0};
+	selectionSort(b,3);
+	for (int i = 0; i < 3; i++)
+	{
+		cout << b[i] << " ";
+	}
+	cout << endl;
+
+	string b1[3] = { "awe","a","asd" };
+	selectionSort(b1, 3);
+	for (int i = 0; i < 3; i++)
+	{
+		cout << b1[i].c_str() << " ";
+	}
+	cout << endl;
+
+
+	Student b2[3] = { {"A",45},{ "B",65 },{ "C",0 } };
+	selectionSort(b2, 3);
+	for (int i = 0; i < 3; i++)
+	{
+		cout << b2[i];
 	}
 	cout << endl;
 	system("pause");
