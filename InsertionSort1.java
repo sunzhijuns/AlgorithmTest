372c2e73addc1e761a780f84edf3648ce4f9ee1e
sunzhijuns <2843359242@qq.com>
2017-11-22 17:38:22 +0800

    优化插入排序

--- InsertionSort1.java
+++ InsertionSort1.java
@@ -11,15 +11,15 @@ public class InsertionSort1{
             // 寻找元素arr[i]合适的插入位置
 
             // 写法1
-            for( int j = i ; j > 0 ; j -- )
-                if( arr[j].compareTo( arr[j-1] ) < 0 )
-                    swap( arr, j , j-1 );
-                else
-                    break;
+//            for( int j = i ; j > 0 ; j -- )
+//                if( arr[j].compareTo( arr[j-1] ) < 0 )
+//                    swap( arr, j , j-1 );
+//                else
+//                    break;
 
             // 写法2
-//            for( int j = i; j > 0 && arr[j].compareTo(arr[j-1]) < 0 ; j--)
-//                swap(arr, j, j-1);
+            for( int j = i; j > 0 && arr[j].compareTo(arr[j-1]) < 0 ; j--)
+                swap(arr, j, j-1);
 
         }
     }
--- SortTestHelper.java
+++ SortTestHelper.java
@@ -1,5 +1,6 @@
 import java.lang.reflect.Method;
 import java.lang.Class;
+import java.util.Random;
 
 public class SortTestHelper {
 
@@ -15,6 +16,28 @@ public class SortTestHelper {
 
         for (int i = 0; i < n; i++)
             arr[i] = new Integer((int)(Math.random() * (rangeR - rangeL + 1) + rangeL));
+        return arr;
+    }
+
+    // 生成一个近乎有序的数组
+    // 首先生成一个含有[0...n-1]的完全有序数组, 之后随机交换swapTimes对数据
+    // swapTimes定义了数组的无序程度:
+    // swapTimes == 0 时, 数组完全有序
+    // swapTimes 越大, 数组越趋向于无序
+    public static Integer[] generateNearlyOrderedArray(int n, int swapTimes){
+
+        Integer[] arr = new Integer[n];
+        for( int i = 0 ; i < n ; i ++ )
+            arr[i] = new Integer(i);
+
+        for( int i = 0 ; i < swapTimes ; i ++ ){
+            int a = (int)(Math.random() * n);
+            int b = (int)(Math.random() * n);
+            int t = arr[a];
+            arr[a] = arr[b];
+            arr[b] = t;
+        }
+
         return arr;
     }
 
