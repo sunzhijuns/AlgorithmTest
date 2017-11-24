198fe3868c2d0625cc3a820f04a0e8321d02c535
sunzhijuns <2843359242@qq.com>
2017-11-25 01:06:53 +0800

    层序遍历

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 #include"SequenceST.h"
 #include<ctime>
 #include<stack>
+#include<queue>
 using namespace std;
 
 template<typename Key, typename Value>
@@ -218,9 +219,9 @@ private:
 		{
 			return;
 		}
-		preOrder(node->left);
+		inOrder(node->left);
 		cout << node->value << endl;
-		preOrder(node->right);
+		inOrder(node->right);
 	}
 	// 递归的后序遍历
 	void postOrder(Node *node) {
@@ -228,8 +229,8 @@ private:
 		{
 			return;
 		}
-		preOrder(node->left);
-		preOrder(node->right);
+		postOrder(node->left);
+		postOrder(node->right);
 		cout << node->value << endl;
 	}
 	// 递归的删除树
@@ -252,10 +253,10 @@ public:
 	}
 
 	bool contain(Key key) {
-		return __contain(root, key);
+		return contain(root, key);
 	}
 	Value* search(Key key) {
-		return __search(root, key);
+		return search(root, key);
 	}
 	void preOrder() {
 		preOrder(root);
@@ -266,7 +267,27 @@ public:
 	void postOrder() {
 		postOrder(root);
 	}
-
+	void levelOrder() {
+		queue<Node*>q;
+		if (root != NULL)
+		{
+			q.push(root);
+		}
+		while (!q.empty())
+		{
+			Node *node = q.front();
+			q.pop();
+			cout << node->key << endl;
+			if (node->left)
+			{
+				q.push(node->left);
+			}
+			if (node->right)
+			{
+				q.push(node->right);
+			}
+		}	
+	}
 
 
 public:
@@ -294,69 +315,18 @@ private:
 
 int main() {
 
-	// 使用圣经作为我们的测试用例
-	string filename = "bible.txt";
-	vector<string> words;
-	if (FileOps::readFile(filename, words)) {
-
-		cout << "There are totally " << words.size() << " words in " << filename << endl;
-		cout << endl;
-
-
-		// 测试 BST
-		time_t startTime = clock();
-
-		// 统计圣经中所有词的词频
-		// 注: 这个词频统计法相对简陋, 没有考虑很多文本处理中的特殊问题
-		// 在这里只做性能测试用
-		BST<string, int> bst = BST<string, int>();
-		for (vector<string>::iterator iter = words.begin(); iter != words.end(); iter++) {
-			int *res = bst.search(*iter);
-			if (res == NULL)
-				bst.insert(*iter, 1);
-			else
-				(*res)++;
-		}
-
-		// 输出圣经中god一词出现的频率
-		if (bst.contain("god"))
-			cout << "'god' : " << *bst.search("god") << endl;
-		else
-			cout << "No word 'god' in " << filename << endl;
-
-		time_t endTime = clock();
-
-		cout << "BST , time: " << double(endTime - startTime) / CLOCKS_PER_SEC << " s." << endl;
-		cout << endl;
-
-
-		// 测试顺序查找表 SST
-		startTime = clock();
-
-		// 统计圣经中所有词的词频
-		// 注: 这个词频统计法相对简陋, 没有考虑很多文本处理中的特殊问题
-		// 在这里只做性能测试用
-		SequenceST<string, int> sst = SequenceST<string, int>();
-		for (vector<string>::iterator iter = words.begin(); iter != words.end(); iter++) {
-			int *res = sst.search(*iter);
-			if (res == NULL)
-				sst.insert(*iter, 1);
-			else
-				(*res)++;
-		}
-
-		// 输出圣经中god一词出现的频率
-		if (sst.contain("god"))
-			cout << "'god' : " << *sst.search("god") << endl;
-		else
-			cout << "No word 'god' in " << filename << endl;
-
-		endTime = clock();
-
-		cout << "SST , time: " << double(endTime - startTime) / CLOCKS_PER_SEC << " s." << endl;
+	BST<int, int> bst = BST<int, int>();
+	for (int i = 0; i < 5; i++)
+	{
+		bst.insert(i, i);
 	}
+	bst.insert(23, 23);
+	bst.insert(8,8);
+	bst.insert(-5, -5);
 
-	return 0;
+	bst.inOrder();
+	bst.levelOrder();
+	
 	system("pause");
 	return 0;
 }
